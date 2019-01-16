b021bc1cfce6928632b406154814af1fcc9488f2
LunaTulande <lunamar91@gmail.com>
2019-01-16 13:07:54 +0100

    Add rigth menu as component and create a shiny-list component

--- ShinyPokemon/ClientApp/src/app/app.module.ts
+++ ShinyPokemon/ClientApp/src/app/app.module.ts
@@ -2,23 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomePokemonComponent } from './home-pokemon/home-pokemon.component';
+import { ShinyListComponent } from './shiny-list/shiny-list.component';
+import { SelectMenuComponent } from './select-menu/select-menu.component';
+
+const appRoutes: Routes = [
+  {path: 'shiny-list', component: ShinyListComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    HomePokemonComponent
+    HomePokemonComponent,
+    ShinyListComponent,
+    SelectMenuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -28,6 +34,7 @@ import { HomePokemonComponent } from './home-pokemon/home-pokemon.component';
       { path: '', component: HomePokemonComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'shiny-list', component: ShinyListComponent },
     ])
   ],
   providers: [],
---
+++ ShinyPokemon/ClientApp/src/app/select-menu/select-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-menu',
+  templateUrl: './select-menu.component.html',
+  styleUrls: ['./select-menu.component.css']
+})
+export class SelectMenuComponent {
+  
+}
