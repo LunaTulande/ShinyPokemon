859b7d3cca41a75270524f64dc0e138448d30191
LunaTulande <lunamar91@gmail.com>
2019-05-29 21:27:36 +0200

    Remove testing files

--- ShinyPokemon/ClientApp/src/app/utils/user-pokemons.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserPokemonsPipe } from './user-pokemons.pipe';
-
-describe('UserPokemonsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UserPokemonsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
